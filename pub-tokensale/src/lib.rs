6fc83f5609ce8bfb79342adafc610c2fc006719e
sonny vesali <sonny.vesali@proton.me>
2022-05-24 16:34:57 -0500

    bug fixes

--- pub-tokensale/src/lib.rs
+++ pub-tokensale/src/lib.rs
@@ -1,30 +1,40 @@
 use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::{ValidAccountId, U128};
-use near_sdk::{env, log, near_bindgen, AccountId, PanicOnDefault, Promise, PromiseOrValue};
+use near_sdk::{
+    env, log, near_bindgen, result_serializer, AccountId, Balance, PanicOnDefault, Promise,
+    PromiseOrValue,
+};
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct PubTokenSale {
-    is_open: bool,
-    native_token_price: U128,
+    // is_open: bool,
+    // native_token_price: U128,
     native_token_account_id: AccountId,
-    native_token_balance: U128,
-    swap_token_account_id: AccountId,
-    swap_token_price: U128,
-    swap_token_balance: U128,
-    wallet_cap_limit: U128,
-    proceeds_address: AccountId,
+    native_token_balance: u128,
+    // swap_token_account_id: AccountId,
+    // swap_token_price: U128,
+    swap_token_balance: u128,
+    // wallet_cap_limit: U128,
+    // proceeds_address: AccountId,
 }
 
 #[near_bindgen]
 impl PubTokenSale {
+    #[result_serializer(borsh)]
+    pub fn new() -> Self {
+        Self {
+            native_token_account_id: "token.test.near".to_string(),
+            native_token_balance: 0,
+            swap_token_balance: 0,
+        }
+    }
+
     #[payable]
     pub fn perform_swap(&mut self) {
-        self.swap_token_balance =
-            U128(u128::from(self.swap_token_balance.0) + env::attached_deposit());
-        self.native_token_balance =
-            U128(u128::from(self.native_token_balance.0) + env::attached_deposit());
+        self.swap_token_balance = u128::from(self.swap_token_balance) + env::attached_deposit();
+        self.native_token_balance = u128::from(self.native_token_balance) + env::attached_deposit();
         Promise::new(self.native_token_account_id.to_string()).transfer(env::attached_deposit());
     }
 }
@@ -42,12 +52,14 @@ impl FungibleTokenReceiver for PubTokenSale {
             &self.native_token_account_id,
             "Only supports deposits from the 1 Token Address"
         );
-        self.native_token_balance = amount;
+        self.native_token_balance = u128::from(amount);
         log!("the amount of tokens in this address is {}", amount.0);
         PromiseOrValue::Value(U128::from(0))
     }
 }
 
-// second we're going to add a function that accepts near and then dispenses 1 of the tokens 1:1 for simplicity ===>
-
-// then you add oracle functionality that then calcultes the amount being dispensed and then that's it for the MVP then we move on to testing it
+/*
+I am almost positive that this doesn't work as intended let's deploy some of these
+to kurtosis and see how they behave and we can move on from there and see
+where we're fucking  this up
+*/
